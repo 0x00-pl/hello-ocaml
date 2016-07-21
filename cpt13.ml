876963b2961f5a4d60b2b7c9e2b6c57584a475d2
0x00-pl <0x00.pl@gmail.com>
2016-07-21 23:37:53 +0800

    modified:   cpt13.ml

--- cpt13.ml
+++ cpt13.ml
@@ -92,14 +92,73 @@ end
    ABORT
    *)
 
+;;
 
 
+open Core.Std
+
+let week_days_= weekdays
+
+let finded2 = List.Assoc.find weekdays 2
 
 
+type t = int String.Map.t
+
+let empty = String.Map.empty
+
+let to_list a = Map.to_alist a
+
+let touch a s =
+  let count =
+    match Map.find a s with
+    | None -> 0
+    | Some x -> x
+  in
+  Map.add a ~key:s ~data:(count + 1)
+
+;;
+let _ = match Map.of_alist Int.comparator weekdays with
+  | `Duplicate_key x -> ()
+  | `Ok weekdays_map ->
+
+    let _weekdays_map_fix6 = Map.add weekdays_map 6 "sunday" in ()
+
+let left = String.Map.of_alist_exn ["foo",1; "bar",3; "snoo",0]
+let right = String.Map.of_alist_exn ["foo",0; "snoo",0]
+let lr_diff = Map.symmetric_diff left right Int.equal
+
+module ReverseStringComparator = Comparator.Make(struct
+    type t = string
+    let sexp_of_t = String.sexp_of_t
+    let t_of_sexp = String.t_of_sexp
+    let compare x y = String.compare y x
+  end)
+
+let comparator_reverse = ReverseStringComparator.comparator
+
+let ord_map = Map.of_alist_exn String.comparator ["foo",1; "bar",3; "snoo",0]
+let ord_tree = Map.to_tree ord_map
+
+let left_snoo = Map.Tree.find String.comparator ord_tree "snoo"
 
 
+let ord_comparator_poly = Map.of_alist_exn Comparator.Poly.comparator ["foo",1; "bar",3; "snoo",0]
+let ord_map_poly = Map.Poly.of_alist_exn ["foo",1; "bar",3; "snoo",0]
 
+let dedup ~comparator l =
+  (List.fold l ~init:(Set.empty comparator) ~f:Set.add)
+  |> Set.to_list
 
+let ex_dedup = dedup ~comparator:(Int.comparator) [1;1;1;1;3;3;3;3;5;5;5;5]
+;;
+
+Caml.Hashtbl.hash (List.range 0 9) |> print_int;
+(* hash List if length over 10 is only hash first 10 element *)
+Caml.Hashtbl.hash (List.range 0 10) |> print_int;
+Caml.Hashtbl.hash (List.range 0 11) |> print_int;
+Caml.Hashtbl.hash (List.range 0 100) |> print_int;
+
+(* Map.change will preserve old data for copies, Hashtbl.change will break old data but faster *)
 
 
 
