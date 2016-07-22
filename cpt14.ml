88d6577f82455280b06dc132f1947dceefe30cb1
0x00-pl <0x00.pl@gmail.com>
2016-07-23 01:28:37 +0800

    Command.run seems opend another process or thread and prvent exception chain.

--- cpt14.ml
+++ cpt14.ml
@@ -13,13 +13,46 @@ let () =
 
 open Core.Std
 
-let do_hash path =
-  In_channel.with_file path ~f:(fun ic ->
-      Digest.input ic
-      |> Digest.to_hex
-      |> print_endline
-  )
+let do_hash path () =
+  try
+    In_channel.with_file path ~f:(fun ic ->
+        Digest.input ic
+        |> Digest.to_hex
+        |> print_endline
+      )
+  with Sys_error _ -> raise Not_found (* TODO: only can catch exception in here *)
 
-let () =
-  do_hash "cpt14.ml"
+let spec =
+  let open Command.Spec in
+  empty
+  +> anon ("filename" %: string)
+
+let command =
+  Command.basic
+    ~summary:"print MD5 of file."
+    ~readme:(fun () -> "this line is more info of MD5 and this command.")
+    Command.Spec.(empty +> anon ("filename" %: file))
+    do_hash
+
+let f () =
+  (* TODO: cannt handle exception in Command.run *)
+  Command.run
+    ~version:"0.1"
+    ~build_info:"RWO"
+    command
+
+let _ = f ()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
