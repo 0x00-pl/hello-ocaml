f7e4909abf73368653ba3d78ec8769358091fec3
0x00-pl <0x00.pl@gmail.com>
2016-07-24 20:16:46 +0800

    use corebuild

---
+++ cpt15/cpt15_t.mli
@@ -0,0 +1,12 @@
+(* Auto-generated from "cpt15.atd" *)
+
+
+type org = {
+  login: string;
+  id: int;
+  url: string;
+  name: string option;
+  blog: string option;
+  email: string option;
+  public_repos: int
+}
